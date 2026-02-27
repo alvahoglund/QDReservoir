4aac4327c4cec44849cbc21a835b5588c78e218d
alvahoglund <hoglundalva@gmail.com>
2026-02-27 15:00:25 +0100

    Update time evolution

--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -1,21 +1,26 @@
+## Standard time evolution
 propagator(t, hamiltonian) = cis(-t * Matrix(hamiltonian))
 
-function propagator(t, hamiltonian, qn, H)
-    #Returns a propagator in H only evolving FermionicHilbertSpaces.indices corresponding to the specified qn
-    index = FermionicHilbertSpaces.indices(qn, H)
+operator_time_evolution(propagator, operator) = propagator' * operator * propagator
+operator_time_evolution(operator, t, hamiltonian) = operator_time_evolution(propagator(t, hamiltonian), operator)
+
+state_time_evolution(propagator, ρ) = propagator * ρ * propagator'
+state_time_evolution(ρ, t, hamiltonian) = state_time_evolution(propagator(t, hamiltonian), ρ)
+
+## Only evolve a qn-sector
+
+propagator(t, hamiltonian, qn::Number, H::AbstractHilbertSpace) = propagator(t, hamiltonian, sector(qn, H), H)
+function propagator(t, hamiltonian{T}, H_qn::AbstractHilbertSpace, H ::AbstractHilbertSpace) where T
+    index = FermionicHilbertSpaces.indices(H_qn, H)
     hamiltonian_sub = hamiltonian[index, index]
     propagator_sub = propagator(t, hamiltonian_sub)
-    propagator_padded = spzeros(Complex{Float64}, dim(H), dim(H))
+    propagator_padded = spzeros(T, dim(H), dim(H))
     propagator_padded[index, index] = propagator_sub
     return propagator_padded
 end
 
+operator_time_evolution(operator, t, hamiltonian, qn, H) = operator_time_evolution(propagator(t, hamiltonian, sector(qn, H), H), operator)
+operator_time_evolution(operator, t, hamiltonian, H_qn, H) = operator_time_evolution(propagator(t, hamiltonian, H_qn, H), operator)
 
-operator_time_evolution(propagator, operator) = propagator' * operator * propagator
-operator_time_evolution(operator, t, hamiltonian) = operator_time_evolution(propagator(t, hamiltonian), operator)
-operator_time_evolution(operator, t, hamiltonian, qn, H) = operator_time_evolution(propagator(t, hamiltonian, qn, H), operator)
-
-
-state_time_evolution(propagator, ρ) = propagator * ρ * propagator'
-state_time_evolution(ρ, t, hamiltonian) = state_time_evolution(propagator(t, hamiltonian), ρ)
-state_time_evolution(ρ, t, hamiltonian, H, qn) = state_time_evolution(propagator(t, hamiltonian, qn, H), ρ)
+state_time_evolution(ρ, t, hamiltonian, qn, H) = state_time_evolution(propagator(t, hamiltonian, sector(qn,H), H), ρ)
+state_time_evolution(ρ, t, hamiltonian, H_qn, H) = state_time_evolution(propagator(t, hamiltonian, H_qn, H), ρ)

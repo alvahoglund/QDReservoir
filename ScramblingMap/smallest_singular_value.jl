496f330e1f08d1bd4de5fc4ba8b0abbe49b1aa69
alvahoglund <hoglundalva@gmail.com>
2026-03-24 13:23:56 +0100

    Update parametes in plot of smallest singular values

--- ScramblingMap/smallest_singular_value.jl
+++ ScramblingMap/smallest_singular_value.jl
@@ -16,10 +16,9 @@ function get_ham(grids, ϵ_func_main, ϵ_func_res, ϵb_func,
         reservoir_parameters, interaction_parameters)
 end
 
-function get_ham(grids)
-    ϵ_func_main, ϵ_func_res, ϵb_func, u_intra_func, t_func, t_so_func, u_inter_func = set_parameters()
-    return get_ham(grids, ϵ_func_main, ϵ_func_res, ϵb_func,
-        u_intra_func, t_func, t_so_func, u_inter_func)
+function get_ham(grids, parameters)
+    get_ham(grids, parameters.ϵ_func_main, parameters.ϵ_func_res, parameters.ϵb_func,
+        parameters.u_intra_func, parameters.t_func, parameters.t_so_func, parameters.u_inter_func)
 end
 
 function avg_smallest_sv(grid, qn_res, randomized_hams, t, measurements)
@@ -40,14 +39,14 @@ function avg_smallest_sv(grid, qn_res, randomized_hams, t, measurements)
     return mean, std
 end
 
-function avg_smallest_sv(nbr_dots_res_list, t, nbr_samples)
+function avg_smallest_sv(nbr_dots_res_list, t, nbr_samples, parameters)
     nbr_dots_main = 2
     avg_smallest_sv_dict = Dict{Tuple{Int, Int}, Tuple{Float64, Float64}}()
     for nbr_dots_res in nbr_dots_res_list
         println("Calculating for reservoir dots: $(nbr_dots_res)")
         grid = QDR.generate_grid(nbr_dots_main, nbr_dots_res)
         measurements = QDR.charge_probabilities(grid.total)
-        randomized_hams = [get_ham(grid) for _ in 1:nbr_samples]
+        randomized_hams = [get_ham(grid, parameters) for _ in 1:nbr_samples]
         for qn_res in 0:(2 * nbr_dots_res)
             mean, std = avg_smallest_sv(grid, qn_res, randomized_hams, t, measurements)
             avg_smallest_sv_dict[(nbr_dots_res, qn_res)] = (mean, std)
@@ -55,6 +54,7 @@ function avg_smallest_sv(nbr_dots_res_list, t, nbr_samples)
     end
     return avg_smallest_sv_dict
 end
+
 function plot_avg_sv_vs_qn(nbr_dots_res, sv_qn, ax)
     means = clean_val.(getindex.(sv_qn, 1))
 
@@ -74,7 +74,8 @@ function plot_avg_sv_vs_qn(avg_smallest_sv_dict, title)
         ylabel = "Average smallest singular value",
         xlabel = "Electrons in reservoir",
         title = title,
-        titlesize = 24
+        titlesize = 24,
+        yscale = log10
     )
 
     for nbr_dots_res in nbr_dots_res_list
@@ -90,22 +91,21 @@ end
 seed = 29084
 Random.seed!(seed)
 
-function set_parameters()
-    ϵ_func_main() = 0.5
-    ϵ_func_res() = rand()
-    ϵb_func() = [0, 0, 1]
-    u_intra_func() = 10 + rand()
-    t_func() = rand()
-    t_so_func() = 0.1 * rand()
-    u_inter_func() = rand()
-    return ϵ_func_main, ϵ_func_res, ϵb_func, u_intra_func, t_func, t_so_func, u_inter_func
-end
+parameters1 = (
+    ϵ_func_main = () -> 0.5,
+    ϵ_func_res = () -> rand(),
+    ϵb_func = () -> [0, 0, 1],
+    u_intra_func = () -> 10 + rand(),
+    t_func = () -> rand(),
+    t_so_func = () -> 0.1 * rand(),
+    u_inter_func = () -> rand()
+)
 
 nbr_dots_res_list = [2, 3, 4, 5, 6]
 t = [100, 200]
-nbr_samples = 10
+nbr_samples = 1
 
-avg_smallest_sv_dict_1 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples)
+avg_smallest_sv_dict_1 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples, parameters1)
 
 title = L"H = H_ϵ + H_B + H_{U}^{\text{intra}} +H_{U}^{\text{inter}} + H_t + H_{SO}"
 plot_avg_sv_vs_qn(avg_smallest_sv_dict_1, title)
@@ -113,22 +113,21 @@ plot_avg_sv_vs_qn(avg_smallest_sv_dict_1, title)
 seed = 42879
 Random.seed!(seed)
 
-function set_parameters()
-    ϵ_func_main() = 0.5
-    ϵ_func_res() = rand()
-    ϵb_func() = [0, 0, 1]
-    u_intra_func() = 10 + rand()
-    t_func() = rand()
-    t_so_func() = 0.1 * rand()
-    u_inter_func() = 0
-    return ϵ_func_main, ϵ_func_res, ϵb_func, u_intra_func, t_func, t_so_func, u_inter_func
-end
+parameters2 = (
+    ϵ_func_main = () -> 0.5,
+    ϵ_func_res = () -> rand(),
+    ϵb_func = () -> [0, 0, 1],
+    u_intra_func = () -> 10 + rand(),
+    t_func = () -> rand(),
+    t_so_func = () -> 0.1 * rand(),
+    u_inter_func = () -> 0
+)
 
 nbr_dots_res_list = [2, 3, 4, 5, 6]
 t = [100, 200]
 nbr_samples = 10
 
-avg_smallest_sv_dict_2 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples)
+avg_smallest_sv_dict_2 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples, parameters2)
 title = L"H = H_ϵ + H_B + H_{U}^{\text{intra}} + H_t + H_{SO}"
 plot_avg_sv_vs_qn(avg_smallest_sv_dict_2, title)
 
@@ -136,22 +135,21 @@ plot_avg_sv_vs_qn(avg_smallest_sv_dict_2, title)
 seed = 298478
 Random.seed!(seed)
 
-function set_parameters()
-    ϵ_func_main() = 0.5
-    ϵ_func_res() = rand()
-    ϵb_func() = [0, 0, 0]
-    u_intra_func() = 10 + rand()
-    t_func() = rand()
-    t_so_func() = 0.1 * rand()
-    u_inter_func() = rand()
-    return ϵ_func_main, ϵ_func_res, ϵb_func, u_intra_func, t_func, t_so_func, u_inter_func
-end
+parameters3 = (
+    ϵ_func_main = () -> 0.5,
+    ϵ_func_res = () -> rand(),
+    ϵb_func = () -> [0, 0, 0],
+    u_intra_func = () -> 10 + rand(),
+    t_func = () -> rand(),
+    t_so_func = () -> 0.1 * rand(),
+    u_inter_func = () -> rand()
+)
 
 nbr_dots_res_list = [2, 3, 4, 5, 6]
 t = [100, 200]
 nbr_samples = 10
 
-avg_smallest_sv_dict_3 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples)
+avg_smallest_sv_dict_3 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples, parameters3)
 title = L"H = H_ϵ + H_{U}^{\text{intra}} + H_{U}^{\text{inter}} + H_t + H_{SO}"
 plot_avg_sv_vs_qn(avg_smallest_sv_dict_3, title)
 
@@ -159,21 +157,20 @@ plot_avg_sv_vs_qn(avg_smallest_sv_dict_3, title)
 seed = 12830
 Random.seed!(seed)
 
-function set_parameters()
-    ϵ_func_main() = 0.5
-    ϵ_func_res() = rand()
-    ϵb_func() = [0, 0, 1]
-    u_intra_func() = 10 + rand()
-    t_func() = rand()
-    t_so_func() = 0
-    u_inter_func() = rand()
-    return ϵ_func_main, ϵ_func_res, ϵb_func, u_intra_func, t_func, t_so_func, u_inter_func
-end
+parameters4 = (
+    ϵ_func_main = () -> 0.5,
+    ϵ_func_res = () -> rand(),
+    ϵb_func = () -> [0, 0, 1],
+    u_intra_func = () -> 10 + rand(),
+    t_func = () -> rand(),
+    t_so_func = () -> 0,
+    u_inter_func = () -> rand()
+)
 
 nbr_dots_res_list = [2, 3, 4, 5, 6]
 t = [100, 200]
 nbr_samples = 10
 
-avg_smallest_sv_dict_4 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples)
+avg_smallest_sv_dict_4 = avg_smallest_sv(nbr_dots_res_list, t, nbr_samples, parameters4)
 title = L"H = H_ϵ + H_B + H_{U}^{\text{intra}} + H_{U}^{\text{inter}} + H_t"
 plot_avg_sv_vs_qn(avg_smallest_sv_dict_4, title)

d8b5f2083921014338f95f258f0c866f19b262c3
alvahoglund <hoglundalva@gmail.com>
2026-04-17 13:01:58 +0200

    Multithread scrambling map singular value

--- ScramblingMap/smallest_singular_value.jl
+++ ScramblingMap/smallest_singular_value.jl
@@ -2,7 +2,7 @@ using QDReservoir, LinearAlgebra, Random, Statistics
 import QDReservoir as QDR
 
 # Set BLAS to single-threaded to avoid oversubscription
-#BLAS.set_num_threads(1)
+BLAS.set_num_threads(1)
 ## ===================== Functions ========================
 clean_val(y) = map(x -> abs(x) < 1e-14 ? NaN
                         : x, y)
@@ -23,11 +23,9 @@ function get_ham(grids, parameters)
         parameters.u_intra_func, parameters.t_func, parameters.t_so_func, parameters.u_inter_func)
 end
 
-function smallest_sv(sys, m_ops, ham_symb, t)
-    hams_matrix = QDR.matrix_representation_hams(ham_symb, sys)
-    ψ_res = ground_state(hams_matrix.res)
+function smallest_sv(sys, m_ops, hams_matrix, ψ_ground, t)
     S = scrambling_map(
-        sys, m_ops, ψ_res, hams_matrix.total, t)
+        sys, m_ops, ψ_ground, hams_matrix.total, t)
     return minimum(svdvals(S))
 end
 
@@ -38,12 +36,18 @@ function avg_sv_vs_qn(nbr_dots_res, t, nbr_samples, parameters)
     measurements = QDR.charge_probabilities(grid.total)
     n_qn = 2 * nbr_dots_res + 1
 
-    sys_list = [tight_binding_system(grid, qn_res) for qn_res in 0:(n_qn - 1)]
-    m_ops_list = [QDR.matrix_representation_ops(measurements, sys.H_total)
-                  for sys in sys_list]
+    sys_list = map(Base.Fix1(tight_binding_system, grid), 0:(n_qn - 1))
+    m_ops_list = map(
+        sys -> QDR.matrix_representation_ops(measurements, sys.H_total), sys_list)
+    hams_symb = [get_ham(grid, parameters) for _ in 1:nbr_samples]
 
-    randomized_hams = [get_ham(grid, parameters) for _ in 1:nbr_samples]
-
+    hams_mat = Matrix{QDR.Hamiltonians}(undef, n_qn, nbr_samples)
+    for idx in CartesianIndices(hams_mat)
+        qn_res_idx, j = Tuple(idx)
+        hams_mat[qn_res_idx, j] = QDR.matrix_representation_hams(
+            hams_symb[j], sys_list[qn_res_idx])
+    end
+    ψ_res = map(ham_mat -> ground_state(ham_mat.res), hams_mat)
     sv_matrix = zeros(Float64, n_qn, nbr_samples)
 
     #Threads.@threads for idx in CartesianIndices(sv_matrix)
@@ -51,7 +55,7 @@ function avg_sv_vs_qn(nbr_dots_res, t, nbr_samples, parameters)
         qn_res_idx, j = Tuple(idx)
         sv_matrix[idx] = smallest_sv(
             sys_list[qn_res_idx], m_ops_list[qn_res_idx],
-            randomized_hams[j], t)
+            hams_mat[qn_res_idx, j], ψ_res[qn_res_idx, j], t)
     end
 
     mean_sv = vec(mean(sv_matrix, dims = 2))
@@ -71,29 +75,37 @@ function avg_sv_vs_res(nbr_dots_res_list, t, nbr_samples, parameters)
 end
 
 ## ================= Smallest singular values vs parameter ========================
+
 function avg_sv_vs_param(nbr_dots_res, qn_res, parameter_list, nbr_samples, t)
     grid = QDR.generate_grid(2, nbr_dots_res)
     measurements = QDR.charge_probabilities(grid.total)
-
+    time1 = time()
     sys = tight_binding_system(grid, qn_res)
     m_ops = QDR.matrix_representation_ops(measurements, sys.H_total)
 
-    hams_symb = [get_ham(grid, parameter_list[i])
-                 for i in eachindex(parameter_list), _ in 1:nbr_samples]
-    #hams_symb = [hamiltonians(grid) for _ in eachindex(parameter_list), _ in 1:nbr_samples]
+    time2 = time()
+    println("Set ops: $(time2 - time1)")
 
-    smallest_svs = Matrix{Float64}(undef, length(parameter_list), nbr_samples)
+    hams_symb = map(
+        Base.Fix1(get_ham, grid), [p for p in parameter_list, _ in 1:nbr_samples])
+    hams_mat = map(Base.Fix2(QDR.matrix_representation_hams, sys), hams_symb)
+    ψ_ground = map(ham_mat -> ground_state(ham_mat.res), hams_mat)
 
-    #Threads.@threads for idx in CartesianIndices(smallest_svs)
-    for idx in CartesianIndices(smallest_svs)
+    time3 = time()
+    println("Hamiltonians and ground state: $(time3- time2)")
+
+    smallest_svs = zeros(Float64, length(parameter_list), nbr_samples)
+
+    Threads.@threads :dynamic for idx in CartesianIndices(smallest_svs)
+        #for idx in CartesianIndices(smallest_svs)
         i, j = Tuple(idx)
-        smallest_svs[idx] = smallest_sv(sys, m_ops, hams_symb[i, j], t)
+        smallest_svs[idx] = smallest_sv(sys, m_ops, hams_mat[i, j], ψ_ground[i, j], t)
     end
 
-    mean_sv = [sum(smallest_svs[i, :]) / nbr_samples for i in 1:length(parameter_list)]
-    std_sv = [sqrt(sum(smallest_svs[i, j]^2 for j in 1:nbr_samples) / nbr_samples -
-                   mean_sv[i]^2)
-              for i in 1:length(parameter_list)]
+    time4 = time()
+    println("Singular values: $(time4-time3)")
+    mean_sv = vec(mean(smallest_svs, dims = 2))
+    std_sv = vec(std(smallest_svs, dims = 2, corrected = false))
 
     return mean_sv, std_sv
 end

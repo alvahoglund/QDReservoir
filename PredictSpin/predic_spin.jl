49436b2931332d1e9e7dae95b58dbdd52ec7066d
alvahoglund <hoglundalva@gmail.com>
2026-05-08 15:01:35 +0200

    Update plots showing performance for different tasks against SSV

--- PredictSpin/predic_spin.jl
+++ PredictSpin/predic_spin.jl
@@ -57,23 +57,24 @@ function plot_varying_noise(Ω, S, Pm, Pm_dict, ps_list)
     end
     display(fig)
 end
-
+#
 # ====== Choose system parameters ======
-nbr_dots_res = 6
-qn_res = 3
-sys = tight_binding_system(2, nbr_dots_res, qn_res)
-seed = 1234
-hams = QDR.matrix_representation_hams(QDR.hamiltonians(sys.grids, seed), sys)
-ρ_res = ground_state(hams.res)
-t_list = [100]
-
-nbr_states = 10000
-measurements = QDR.charge_measurements(sys)
-
-Pm, Pm_dict = QDR.pauli_matrix(sys.Hs_main, sys.H_main)
-Ω = stack(vec(QDR.hilbert_schmidt_ensemble(sys.H_main)) for i in 1:nbr_states)
-S = scrambling_map(sys, measurements, ρ_res, hams.total, t_list)
-
+#br_dots_res = 6
+#n_res = 3
+#ys = tight_binding_system(2, nbr_dots_res, qn_res)
+#eed = 1234
+#ams = QDR.matrix_representation_hams(QDR.hamiltonians(sys.grids, seed), sys)
+#_res = ground_state(hams.res)
+#_list = [100]
+#
+#br_states = 10000
+#easurements = QDR.charge_measurements(sys)
+#
+#m, Pm_dict = QDR.pauli_matrix(sys.Hs_main, sys.H_main)
+# = stack(vec(QDR.hilbert_schmidt_ensemble(sys.H_main)) for i in 1:nbr_states)
+# = scrambling_map(sys, measurements, ρ_res, hams.total, t_list)
+#
 # ===== PLOT VARYING NOISE =========== 
-ps_list = [(:σz, :σz)]
-plot_varying_noise(Ω, S, Pm, Pm_dict, ps_list)
+#s_list = [(:σz, :σz)]
+#lot_varying_noise(Ω, S, Pm, Pm_dict, ps_list)
+#
